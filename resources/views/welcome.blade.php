5d69d500f86e718c2c975c58f130f63f9ac4dd3f
Marcelo Magalhães <marcelof03@hotmail.com>
2017-11-12 18:20:31 -0200

    Alterando nome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    UnBer
                 </div>
 
                 <div class="links">
